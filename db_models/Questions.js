3e711dc9cfcdc838c9a38511e44da7fb9d410cbb
nomis <milan-b@gmx.net>
2018-07-08 00:39:11 +0200

    Add all recent work

---
+++ db_models/Questions.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const QuestionsSchema = new schema({
+    question: {type: String, required: true},
+    answer: {type: String},
+    topic: {type: String, required: true},
+    url: {type: String, required: true},
+    tags: {type: Array},
+    points:{type: Number}
+})
+
+mongoose.model("Questions", QuestionsSchema)
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mongoose = require("mongoose")
+const data = require("./lib/data")
+
+// Connect to MongoDB
+mongoose.Promise = global.Promise  // Mongoose Promise is depricated
+mongoose.connect("mongodb://localhost:27017/adcq")
+    .then(() => {
+        console.log(
+            "Cennection to Mongo DB successfull."
+        );
+    })
+    .catch((err) => console.log(err))
+
+
+// Initialize server
+let app = express()
+
+// Configure template-engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
+
+// Define routes
+app.get("/", (req, res) => {
+    console.log(req.query);
+    /*
+    data.getQuestions()
+        .then((questions)=>{
+            questions.sortByTopic()
+        }).then((sortetQuestions) => {
+            res.render("home", {questions: sortetQuestions})
+        })
+    */
+    res.render("home")
+})
+
+app.get("/add_question/", (req, res) => {
+    res.render("add_question")
+})
+
+// Start server
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+
+})
+
